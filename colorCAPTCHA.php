cae0d13a957fb8815bc6664e1f2a1c7540207218
Bruce Nguyen <khoanguyendnvn@gmail.com>
2015-10-20 17:50:07 +0700

    working version the real one?MAY BE....

--- colorCAPTCHA.php
+++ colorCAPTCHA.php
@@ -138,7 +138,6 @@ $_SESSION['randomColor'] = $randomColor;
 		<script src="js/jquery-1.10.2.js"></script>
 		<script scr="js/jquery-ui-1.10.4.custom.min.js"></script>
 		<script>
-			//javascript goes here
 			$(document).ready(function()
 			{
 				$('button').button();
